85133d7e81a74817ae1f1a8bc946c65cbc2f7e74
Ruben van Rijen <rubenvanrijen@gmail.com>
2023-09-17 21:17:45 +0200

    header and footer made for all the pages

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  public currentYear: number;
+  public applicationName = environment.applicationName;
+  
+  public dragonSkyrimImage = "assets/standart-stickers/skyrim-dragon.jpg"
+  public arrowSkyrimImage = "assets/standart-stickers/skyrim-arrow.jpg"
+  public pikachuImage = "assets/standart-stickers/pikachu.jpg"
+  
+  
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
+  
+  ngOnInit(): void {
+  }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  public logoImage: string = "assets/standart-stickers/stickerwave-logo.jpg"
+  public applicationName: string = environment.applicationName;
+   
+  public isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
